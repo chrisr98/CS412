7229bf043da4d407d65f46cf2b5314f0befd0f7a
chrisr98 <craymond098@gmail.com>
2020-06-24 21:46:54 -0400

    PS5b

--- PS5b/PS4/routes/ps4.js
+++ PS5b/PS4/routes/ps4.js
@@ -3,6 +3,9 @@ const express = require('express');
 const router = express.Router();
 let request = require("request");
 
+const redis = require("redis");
+const client = redis.createClient();
+
 
 router.get('/', function(req, res, next)  {
     let options = {
@@ -11,6 +14,29 @@ router.get('/', function(req, res, next)  {
         headers: {
         }
     };
+
+    client.exists(options, (error, response) => {
+      if(error) { throw new Error(error);}
+      if (response == 1) {
+          client.get(options, (error, response) => {
+              console.table(response);
+              const ans = JSON.parse(response);
+              ans.cached = "cached";
+              res.send(JSON.stringify(ans + 'cached'));
+          })
+
+      } else {
+          const ans = JSON.parse(response);
+          ans.cached = "not cached";
+          client.set(options, ans, (error, response) => {
+              console.table(response);
+              setTimeout(() => { client.del(options,(err, success) => {
+                  if (err) { throw new Error(err)}
+              });}, 30000);
+              res.send(JSON.stringify(ans + 'nit cached'));
+          })
+      }
+    })
     request(options, function (error, response) {
         if (error) throw new Error(error);
         res.render('index', {breeds : JSON.parse(response.body)});
