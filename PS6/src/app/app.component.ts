1e897f5c26859f79ed749a67a4d1e9f46d71acea
chrisr98 <craymond098@gmail.com>
2020-06-27 00:43:22 -0400

    PS6

---
+++ PS6/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { BREEDS} from './Breeds-Mock';
+import { BREED} from './Breeds';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'CS412 Breed Names';
+  breeds = BREEDS;
+  selectedBreed: BREED;
+
+  selectBreed(breed: BREED) {
+    this.selectedBreed = breed;
+  }
+}
