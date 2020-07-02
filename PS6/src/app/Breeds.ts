274edeec7adb5f9572ba905865f6b62216664720
chrisr98 <craymond098@gmail.com>
2020-07-01 21:55:25 -0400

    PS6

--- PS6/src/app/Breeds.ts
+++ PS6/src/app/Breeds.ts
@@ -1,3 +1,53 @@
-export class BREED {
-  name: string;
-}
+import {BREEDS} from './Breeds';
+
+export const BREEDS: BREED[] =
+  [
+    {
+     name: 'affenpinscher',
+    },
+    {
+      name: 'corgi',
+    },
+    {
+      name: 'african',
+    },
+    {
+      name: 'beagle',
+    },
+    {
+      name: 'boxer',
+    },
+    {
+      name: 'norwegian',
+    },
+    {
+      name: 'greyhound',
+    },
+    {
+      name: 'labrador',
+    },
+    {
+      name: 'australian',
+    },
+    {
+      name: 'dachshund',
+    },
+    {
+      name: 'pinscher',
+    },
+    {
+      name: 'borzoi',
+    },
+    {
+      name: 'mastiff',
+    },
+    {
+      name: 'bullterrier',
+    },
+    {
+      name: 'terrier',
+    },
+    {
+      name: 'husky',
+    }
+];
